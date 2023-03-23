380c7007f554410ba624eea66d208c3927af67b4
Theohuub <henrique05162@gmail.com>
2023-03-23 16:15:27 -0300

    Conexão ao Banco de dados

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiTheofiloApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiTheofiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiTheofiloApplication.class, args);
+	}
+
+}
